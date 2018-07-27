8d5a453f692d21d2ad3ad3ab6952eebfbe86feac
sincisco <1158889685@qq.com>
2018-07-27 20:22:39 +0800

    修改

--- src/app/layout/app.header.component.ts
+++ src/app/layout/app.header.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './app.header.component.html',
   styleUrls: ['./app.header.component.less']
 })
-export class AppHeaderComponent {
+export class AppHeaderComponent implements AfterViewInit {
+
+  mouseEnter(event: MouseEvent, id: string) {
+    console.log('mouseEnter');
+    $(`#${id}`).css({left: $(event.currentTarget).offset().left}).show();
+  }
+
+  mouseLeave() {
+    console.log('mouseLeave');
+  }
+
+  ngAfterViewInit() {
+    $('#helperTools').mouseleave(() => {
+      console.log('helperTools mouseLeave');
+      $('#helperTools').hide();
+    });
+
+    $('#filterTools').mouseleave(() => {
+      console.log('helperTools mouseLeave');
+      $('#filterTools').hide();
+    });
+
+    $('#moreTools').mouseleave(() => {
+      console.log('helperTools mouseLeave');
+      $('#moreTools').hide();
+    });
+  }
 
 }
