881a97e35fb7d05a4cedb2abc027f28b847336d0
sincisco <1158889685@qq.com>
2018-10-25 16:01:20 +0800

    删除信息管理部分   保证设计器代码的纯粹性

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
-import { registerLocaleData } from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {NgZorroAntdModule, NZ_I18N, zh_CN} from 'ng-zorro-antd';
+import {registerLocaleData} from '@angular/common';
 import zh from '@angular/common/locales/zh';
-import { LayoutModule } from './layout/layout.module';
-import { SystemModule } from './system/syetem.module';
-import { AppRoutingModule } from './app.routing';
-import { LoginModule } from './login/login.module';
-import { CoreModule } from './guard/core.module';
-import { HeaderInterceptor } from './guard/header-interceptor';
+import {LayoutModule} from './layout/layout.module';
+import {AppRoutingModule} from './app.routing';
 
 registerLocaleData(zh);
 
@@ -25,13 +21,10 @@ registerLocaleData(zh);
     HttpClientModule,
     NgZorroAntdModule,
     LayoutModule,
-    SystemModule,
-    LoginModule,
-    CoreModule,
-    SystemModule,
     AppRoutingModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: zh_CN }, { provide: HTTP_INTERCEPTORS, useClass: HeaderInterceptor, multi: true }],
+  providers: [{provide: NZ_I18N, useValue: zh_CN}],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import {DesignerComponent} from './layout/designer.component';
 
 const routes: Routes = [
   { path: '', component: DesignerComponent },
   { path: 'designer', component: DesignerComponent },
-  { path: 'login', component: LoginComponent },
   { path: '**', component: DesignerComponent }
 ];
 
